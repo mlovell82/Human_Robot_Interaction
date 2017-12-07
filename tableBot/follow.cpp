48ea95191f39293f514f3f2738e55b9034bcc6cb
Mick Lovell <mlovell@kent.edu>
2017-12-07 13:09:29 -0500

    Create follow.cpp
    This program executes a command on the command line to launch the follower package from ROS.  This program is lauched when the follower button is pressed on the tableBot gui.  With this program the user can command the robot to follow them around the room with the click of a button.

---
+++ tableBot/follow.cpp
@@ -0,0 +1,14 @@
+// a program to execute the follower package available with the ROS system
+// created by Michael Lovell
+// 12/8/2017
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+system("roslaunch turtlebot_follower follower.launch");
+
+return 0;
+}
